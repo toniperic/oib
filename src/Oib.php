07540f1960301f659073e7fa4a6f8a27f3f03eb6
Toni Perić <toni.peric@hotmail.com>
2015-04-19 17:27:48 +0200

    Initial commit

---
+++ src/Oib.php
@@ -0,0 +1,45 @@
+<?php namespace ToniPeric;
+
+class Oib
+{
+    const LENGTH = 11;
+
+    /**
+     * Validates a single OIB or an array of OIBs
+     *
+     * @param  array|int $data
+     * @return array|bool
+     */
+    public static function validate($data)
+    {
+        if (is_array($data)) {
+            foreach ($data as $oib) {
+                $result[$oib] = static::check($oib);
+            }
+
+            return $result;
+        }
+
+        return static::check($data);
+    }
+
+    /**
+     * Validates a single OIB
+     *
+     * @param  int $data
+     * @return bool
+     */
+    protected static function check($data)
+    {
+        if (is_numeric($data) && strlen($data) == self::LENGTH) {
+            $a = 10;
+            for ($i = 0; $i < 10; $i++) {
+                $a = $a == 0 ? 10 : ($a + intval(substr($data, $i, 1), 10)) % 10 * 2 % 11;
+            }
+
+            return 11 - $a == 10 ? 0 : 11 - $a == intval(substr($data, 10, 1), 10);
+        }
+
+        return false;
+    }
+}
---
+++ tests/OibTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use ToniPeric\Oib;
+
+class OibTest extends PHPUnit_Framework_TestCase
+ {
+     /**
+      * @test
+      */
+     function it_validates_a_single_oib()
+     {
+         $this->assertEquals(true, Oib::validate(71481280786));
+     }
+
+    /**
+     * @test
+     */
+    function it_invalidates_a_single_oib()
+    {
+        $this->assertEquals(false, Oib::validate('foo'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_validates_an_array_of_oibs()
+    {
+        $oibs = [71481280786, 64217529143, 'foo'];
+
+        $this->assertEquals([$oibs[0] => true, $oibs[1] => true, $oibs[2] => false], Oib::validate($oibs));
+    }
+ }
